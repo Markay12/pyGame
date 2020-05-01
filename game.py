bf7d49d98b26e7f34392d37eaedd08790f6c6cdc
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-05-01 14:38:22 -0700

    main game loop and setup

--- game.py
+++ game.py
@@ -3,8 +3,38 @@ import pygame
 successes, failures = pygame.init()
 print("{0} successes and {1} failures".format(successes, failures))
 
-game = pygame.display.set_mode((600, 400))
+#define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-pygame.display.set_caption("My First Game")
+#open window
+screen = pygame.display.set_mode(700, 500)
+pygame.display.set_caption("MarkPong")
 
-input("press ENTER to exit application\n")
+#loop to keep the game moving
+continueGame = True
+
+#update interval
+updat = pygame.time.Clock()
+
+#---Main loop---
+while continueGame:
+    for event in pygame.event.get(): #actionEvent
+        if event.type == pygame.QUIT:
+            continueGame = False
+
+    #---Main PLAY GAME Loop
+
+    #clear screen to black
+    screen.fill(BLACK)
+
+    #draw net
+    pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+
+    #update screen
+    pygame.display.flip()
+
+    #30fps cap
+    updat.tick(30)
+
+pygame.quit #happens when continue = False
