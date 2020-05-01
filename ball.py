15a2da5687b5ea77b76070a655232eda9c876196
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-05-01 15:17:32 -0700

    ball physics and update method

--- ball.py
+++ ball.py
@@ -1,2 +1,30 @@
 import pygame
 
+from random import randint
+
+BLACK = (0, 0, 0)
+
+class ball(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        #call parent class
+        super().__init__()
+
+        #color and backgrounds
+        self.image = pygame.Surface([width, height]) #instance object
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        #draw rectange ball
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.velocity = [randint(4, 8), randint(-8, 8)] #choose random integers between these ranges
+
+        #fetch object
+        self.rect = self.image.get_rect()
+
+        #---update function---
+
+        def update(self):
+            self.rect.x += self.velocity[0]
+            self.rect.y += self.velocity[1]
