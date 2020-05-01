4143eb24e15f810589fefcf16e690107d91efac3
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-05-01 15:12:17 -0700

    included paddle implementation and main loop

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from paddle import paddle
 
 successes, failures = pygame.init()
 print("{0} successes and {1} failures".format(successes, failures))
@@ -8,8 +9,24 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 #open window
-screen = pygame.display.set_mode(700, 500)
+screen = pygame.display.set_mode((700, 500))
 pygame.display.set_caption("MarkPong")
+
+#implementation of paddles
+leftPaddle = paddle(WHITE, 10, 100)
+leftPaddle.rect.x = 20
+leftPaddle.rect.y = 200
+
+rightPaddle = paddle(WHITE, 10, 100)
+rightPaddle.rect.x = 670
+rightPaddle.rect.y = 200
+
+#list that contains all of the sprites
+sprites_list = pygame.sprite.Group()
+
+#add paddles to list
+sprites_list.add(leftPaddle)
+sprites_list.add(rightPaddle)
 
 #loop to keep the game moving
 continueGame = True
@@ -22,8 +39,27 @@ while continueGame:
     for event in pygame.event.get(): #actionEvent
         if event.type == pygame.QUIT:
             continueGame = False
+        elif event.type == pygame.KEYDOWN:
+            #press ESCAPE to QUIT
+            if event.key == pygame.K_ESCAPE:
+                continueGame = False;
 
     #---Main PLAY GAME Loop
+
+    #move paddles via key
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        leftPaddle.moveUp(5)
+    if keys[pygame.K_s]:
+        leftPaddle.moveDown(5)
+    if keys[pygame.K_UP]:
+        rightPaddle.moveUp(5)
+    if keys[pygame.K_DOWN]:
+        rightPaddle.moveDown(5)
+
+    #Game logic
+    sprites_list.update()        
 
     #clear screen to black
     screen.fill(BLACK)
@@ -31,10 +67,15 @@ while continueGame:
     #draw net
     pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
 
+    #update all sprites
+    sprites_list.draw(screen)
+
     #update screen
     pygame.display.flip()
 
     #30fps cap
     updat.tick(30)
+
+
 
 pygame.quit #happens when continue = False
